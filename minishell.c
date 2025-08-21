7f9d00cce9c402698ef411403db113256aa780de
galvinhuang <galvin.huang@student.adelaide.edu.au>
2025-08-21 15:34:23 +0930

    v0.6.0 minishell.c     Major:     - Added global struct array to retain command for DONE message     - Added helper functions;         - find_index locates index within struct array for a given pid         - resize_BCKProcess helps reallocate the dynamic background array once filled     - Updated child signal handler to print DONE message.
        Minor:
        - Manual cleanup of code comments & formatting.
    
        Context:
        At the moment, waitpid() appears to be interrupted by other signals which break functionality.
    
        - Removed SA_RESTART since it appears to cause unintentional resets, preventing child signal handler from picking up background processes.
        - Added errno == EINTR check to allow waitpid() to continue to prevent "Interrupted with system call"
    
        Future Changes;
        This code is still heavily flawed since it relies on printing the DONE message through the signal handler.
        The functions use is not async-safe.
        Likely need to institute another global array to store pids, then have main function read out the message.

--- minishell.c
+++ minishell.c
@@ -7,38 +7,104 @@
    Compiler/System	: gcc/linux
 
 ********************************************************************/
-#define _POSIX_C_SOURCE 199309L
+#define _POSIX_SOURCE
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <asm-generic/signal-defs.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <stdbool.h>
-#include <asm-generic/signal-defs.h>
+#include <errno.h>
 
-#define NV 20			/* max number of command tokens */
-#define NL 100			/* input buffer size */
-char            line[NL];	/* command input buffer */
+#define NV 20		              /* max number of command tokens */
+#define NL 100	              /* input buffer size */
+#define BACK_PROCESS_SIZE 10  /* size of background process struct array */
+char line[NL];	/* command input buffer */
 
-/*
-	shell prompt
- */
 
-void prompt(void)
-{
-  fflush(stdout);
+/* global background processes*/
+struct BCKProcess {
+  int pid;
+  int job_number;
+  char *command;
+};
+
+/* global array of background structs */
+struct BCKProcess *background_processes, *temp;
+int size;
+
+/* helper function to locate index to 
+struct with command associated with pid 
+*/
+int find_index(int pid) { 
+  for (int i = 0; i < size; i++){
+    if (background_processes[i].pid == pid){
+      return i;
+    }
+  }
+  return -1;
 }
 
-void sigchild(int signum) { 
+/* helper function to resize background struct */
+int resize_BCKProcess(int job) {
+  temp = realloc(background_processes, size * 2);
+  if (temp != NULL){
+    background_processes = temp;
+    size = size * 2;
+  } else {
+    perror("realloc ERROR");
+    return -1;
+    }
+  return 0;
+}
+
+/* shell prompt */
+void prompt(void){fflush(stdout);}
+
+/* waipid waits for any termination of child background processes (-1)
+if status information for at least one process (child) isn't available,
+waitpid() return 0 (WNOHANG)
+*/
+void sigchild(int signum) {
   int status;
   pid_t pid;
-  /* waitpid watiing for any background processes (-1)
-  With WNOHANG was given, if status information is not available for at least one process (child)
-  waitpid() returns a value of 0.
-  */
-  while ((pid = waitpid(-1, &status, WNOHANG)) > 0){}
+
+  while ((pid = waitpid(-1, &status, WNOHANG)) > 0){
+    /* Instruct waitpid to continue when 
+    "Interrupted with system call"
+    */
+    if (errno == EINTR){
+      continue;
+    }
+    
+    int index = find_index(pid);
+    if (index == -1){
+      char *index_message = "FAILED to locate command with pid. \n";
+      return;
+    }
+
+    char done_message[NL + 13];
+    done_message[0] = '[';
+    done_message[1] = '\0';
+    char job[16];
+
+    snprintf(job, sizeof(job), "%d", background_processes[index].job_number);
+    strcat(done_message, "] Done ");
+    strcat(done_message, background_processes[index].command);
+    strcat(done_message, "\n");
+
+    /* Child process exited normally / by signal */
+    if (WIFEXITED(status) == true){
+      write(STDERR_FILENO, done_message, strlen(done_message));
+    } else if (WIFSIGNALED(status) == true){
+      perror("Child Process Ended by Signal");
+      printf("Signal Value: %d\n", WTERMSIG(status));
+    }
+  }
+  return;
 }
 
 /* argk - number of arguments */
@@ -46,36 +112,37 @@ void sigchild(int signum) {
 /* envp - environment pointer */
 int main(int argk, char *argv[], char *envp[])
 {
-  int             frkRtnVal;	    /* value returned by fork sys call */
-  //int             wpid;		        /* value returned by wait */
-  char           *v[NV];	        /* array of pointers to command line tokens */
-  char           *sep = " \t\n";  /* command line token separators    */
-  int             i;		          /* parse index */
-  bool       background = false;  /* boolean to indicate command to be run in background*/
-  int        job_number = 0;      /* arbiturary job number*/
+  int frkRtnVal;	          /* value returned by fork sys call */
+  char *v[NV];	            /* array of pointers to command line tokens */
+  char *sep = " \t\n";      /* command line token separators */
+  int i;		                /* parse index */
+  bool background = false;  /* boolean to indicate command to be run in background */
+  int job_number = 0;       /* arbiturary job number */
 
-  /* prompt for and process one command line at a time  */
-  
-  /* sigaction struct linked to custom sigchild handler
-  sigemptyset to specify no other signals are blocked during custom handler
-  SA_NOCLDSTOP to remove stop/pauses, only consider termination.
-  SA_RESTART to restart interrupted library functions.
+  background_processes = (struct BCKProcess *)malloc(BACK_PROCESS_SIZE * sizeof(struct BCKProcess));
+  if (background_processes == NULL) {
+    perror("malloc ERROR");
+    return -1;
+  }
+  size = BACK_PROCESS_SIZE;
+
+  /* initialise sigaction to custom sigchild signal handler 
+  set empty signal mask of calling process
+  SA_NOCLDSTOP to remove stop/pauses, only consider termination (SIGCHLD)
   */
-  struct sigaction sigact;
-  sigact.sa_handler = sigchild;
-  sigemptyset(&sigact.sa_mask);
-  sigact.sa_flags = SA_RESTART | SA_NOCLDSTOP;
-  sigaction(SIGCHLD, &sigact, NULL);
+  struct sigaction sigact_child;
+  sigact_child.sa_handler = &sigchild;
+  sigemptyset(&(sigact_child.sa_mask));
+  sigact_child.sa_flags = SA_NOCLDSTOP;
 
   while (1) {			/* do Forever */
+    sigaction(SIGCHLD, &sigact_child, NULL);
     prompt();
     fgets(line, NL, stdin);
     fflush(stdin);
 
     if (feof(stdin)) {		/* non-zero on EOF  */
-
-      fprintf(stderr, "EOF pid %d feof %d ferror %d\n", getpid(),
-	      feof(stdin), ferror(stdin));
+      //fprintf(stderr, "EOF pid %d feof %d ferror %d\n", getpid(),feof(stdin), ferror(stdin));
       exit(0);
     }
     if (line[0] == '#' || line[0] == '\n' || line[0] == '\000'){
@@ -97,8 +164,14 @@ int main(int argk, char *argv[], char *envp[])
     background = false;
     if (strcmp(v[size - 1], "&") == 0) {
       v[size - 1] = '\0';
+      size--;
       background = true;
     }
+
+    for (i = 0; i < size; i++){
+      printf("WHY %s\n", v[i]);
+    } 
+
 
     /* detect cd command for proper execution */
     if (strcmp(v[0],"cd") == 0){
@@ -112,7 +185,6 @@ int main(int argk, char *argv[], char *envp[])
           perror("cd command ERROR"); // cd perror message
         }
       }
-      
     } else {
       /* fork a child process to exec the command in v[0] */
       switch (frkRtnVal = fork()) {
@@ -134,20 +206,24 @@ int main(int argk, char *argv[], char *envp[])
         default:			/* code executed only by parent process */
         {
           if (background == true){
-            printf("[%d] %d\n", job_number, getpid());
+            printf("[%d] %d\n", job_number, frkRtnVal);
+            background_processes[job_number].pid = frkRtnVal;
+            background_processes[job_number].job_number = job_number;
+            background_processes[job_number].command = line;
             job_number++;
-            /*
-            printf("[%d] Done ", job_number);
-            for (i = 0; i < size-1; i++){
-              printf("%s ", v[i]);
+            if (job_number == size){
+              if (resize_BCKProcess(job_number) == -1){
+                return -1;
+              }
             }
-            printf("\n");
-            */
+            break;
           } else {
             wait(0);
+            break;
           }
         }
       }				/* switch */
     }      /* else */
   }				/* while */
+  free(background_processes);
 }				/* main */
