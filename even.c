efd49c047c56bb709501c7d12a743f2cac455c7e
galvinhuang <galvin.huang@student.adelaide.edu.au>
2025-08-23 08:18:39 +0930

    v0.5.3b even.c     Added forgotten signal reset
        Considerations;
        - nanosleep() to reset sleep?

--- even.c
+++ even.c
@@ -1,24 +1,18 @@
 #define _POSIX_SOURCE
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <signal.h>
 
 volatile sig_atomic_t sighup_receive = 0;
 volatile sig_atomic_t sigint_receive = 0;
 
 // custom function for HUP signal
-void sighup(int signum) {
-  sighup_receive = 1;
-  //write(STDERR_FILENO, "Ouch!\n", 6);
-}
+void sighup(int signum) { sighup_receive = 1; }
 
 // custom function for INT signal
-void sigint(int signum) {
-  sigint_receive = 1;
-  //write(STDERR_FILENO, "Yeah!\n", 6);
-}
+void sigint(int signum) { sigint_receive = 1; }
 
 int main(int argc, char *argv[]) {
   // initialise sigaction struct.
@@ -42,12 +36,6 @@ int main(int argc, char *argv[]) {
 
   char *endptr;
 
-  /*
-  if (argc != 2){
-    return -1;
-  }
-  */
-  
   // convert command input into long int
   long int n = strtol(argv[1], &endptr, 10);
 
@@ -55,17 +43,18 @@ int main(int argc, char *argv[]) {
   long int i = 0;
   char buffer[16];
   // loop printing even numbers "n" times
-  while (i < n){
-    if (sighup_receive){
+  while (i < n) {
+    if (sighup_receive) {
       write(STDERR_FILENO, "Ouch!\n", 6);
+      sighup_receive = 0;
     } else if (sigint_receive) {
       write(STDERR_FILENO, "Yeah!\n", 6);
+      sigint_receive = 0;
     }
     snprintf(buffer, sizeof(buffer), "%d\n", even);
     write(STDERR_FILENO, buffer, strlen(buffer));
-    //printf("%d\n", even);
     even = even + 2;
-    if (i < n-1){
+    if (i < n - 1) {
       sleep(5);
     }
     i++;
